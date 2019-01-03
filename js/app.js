37c9d3238e20d20c9b7483a1f232b5cec8dd41cc
nosheeno <nosheen@uconnect.pk>
2019-01-03 17:24:59 +0500

    adding indrxTwo.html and app.js files

---
+++ js/app.js
@@ -0,0 +1,20 @@
+let a = document.querySelector("h2");
+let b = a.setAttribute("id","heading");
+let c = a.setAttribute("class","text-primary");
+let d = a.setAttribute("style","some");
+let f = a.setAttribute("type","text");
+let g = a.setAttribute("id","heading");
+console.log(a);
+let h = a.attributes;
+console.log(h)
+let i = a. getAttribute("id");
+console.log(i);
+let j = a. getAttribute("style");
+console.log(j);
+let k = a. getAttribute("class");
+console.log(k);
+
+let m = a.removeAttribute("id");
+console.log(a);
+
+
